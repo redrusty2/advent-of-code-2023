63ae2f326e4c765526735dfdec08d2343317d43c
Caleb Rust <caleb.rust@consilium.technology>
2023-12-05 23:29:30 +1030

    day5 improvements

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -25,46 +25,28 @@ fn part_two(input: String) {
         .unwrap()
         .trim()
         .split_whitespace()
-        .map(|n| n.parse::<i64>().unwrap()).collect();
-
-    lines.nth(1);
-    let seed_to_soil = create_map(&mut lines);
-
-    lines.next();
-    let soil_to_fertilizer = create_map(&mut lines);
-
-    lines.next();
-    let fertilizer_to_water = create_map(&mut lines);
-
-    lines.next();
-    let water_to_light = create_map(&mut lines);
-
-    lines.next();
-    let light_to_temp = create_map(&mut lines);
-
-    lines.next();
-    let temp_to_humid = create_map(&mut lines);
-
-    lines.next();
-    let humid_to_location = create_map(&mut lines);
-
-    let locations: Vec<i64> = seeds
-        .chunks(2)
-        .inspect(|w| println!("{} {}", w[0], w[1]))
-        .flat_map(|w| {
-            (w[0]..w[0] + w[1])
-                .into_iter()
-                .map(|seed| convert(seed, &seed_to_soil))
-                .map(|soil| convert(soil, &soil_to_fertilizer))
-                .map(|fert| convert(fert, &fertilizer_to_water))
-                .map(|water| convert(water, &water_to_light))
-                .map(|light| convert(light, &light_to_temp))
-                .map(|temp| convert(temp, &temp_to_humid))
-                .map(|humid| convert(humid, &humid_to_location))
-        })
+        .map(|n| n.parse::<i64>().unwrap())
         .collect();
 
-    println!("Part two: {}", locations.iter().min().unwrap());
+    lines.next();
+
+    let mut maps = Vec::new();
+    while let Some(_) = lines.next() {
+        maps.push(create_map(&mut lines));
+    }
+
+    let smallest_location = seeds
+        .chunks(2)
+        .inspect(|w| println!("{} {}", w[0], w[1]))
+        .flat_map(|chunk| {
+            (chunk[0]..chunk[0] + chunk[1])
+                .into_iter()
+                .map(|seed| maps.iter().fold(seed, |acc, m| convert(acc, m)))
+        })
+        .min()
+        .unwrap();
+
+    println!("Part two: {}", smallest_location);
 }
 
 fn part_one(input: String) {
@@ -80,38 +62,19 @@ fn part_one(input: String) {
         .split_whitespace()
         .map(|n| n.parse::<i64>().unwrap());
 
-    lines.nth(1);
-    let seed_to_soil = create_map(&mut lines);
+    lines.next();
 
-    lines.nth(1);
-    let soil_to_fertilizer = create_map(&mut lines);
+    let mut maps = Vec::new();
+    while let Some(_) = lines.next() {
+        maps.push(create_map(&mut lines));
+    }
 
-    lines.nth(1);
-    let fertilizer_to_water = create_map(&mut lines);
+    let smallest_location = seeds
+        .map(|seed| maps.iter().fold(seed, |acc, m| convert(acc, &m)))
+        .min()
+        .unwrap();
 
-    lines.nth(1);
-    let water_to_light = create_map(&mut lines);
-
-    lines.nth(1);
-    let light_to_temp = create_map(&mut lines);
-
-    lines.nth(1);
-    let temp_to_humid = create_map(&mut lines);
-
-    lines.nth(1);
-    let humid_to_location = create_map(&mut lines);
-
-    let locations: Vec<i64> = seeds
-        .map(|seed| convert(seed, &seed_to_soil))
-        .map(|soil| convert(soil, &soil_to_fertilizer))
-        .map(|fert| convert(fert, &fertilizer_to_water))
-        .map(|water| convert(water, &water_to_light))
-        .map(|light| convert(light, &light_to_temp))
-        .map(|temp| convert(temp, &temp_to_humid))
-        .map(|humid| convert(humid, &humid_to_location))
-        .collect();
-
-    println!("Part one: {}", locations.iter().min().unwrap());
+    println!("Part one: {}", smallest_location);
 }
 
 fn convert(source: i64, map: &Vec<Pair>) -> i64 {
