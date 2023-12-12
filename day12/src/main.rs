fa836b7ed387df37e4d5f5d327e081dc9cdcab99
Caleb Rust <caleb.rust@consilium.technology>
2023-12-13 08:35:25 +1030

    day12 part 2 really slow

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,46 +1,142 @@
-use std::{collections::VecDeque, fs, iter::repeat};
+use std::{collections::VecDeque, fs, iter::repeat, sync::mpsc::channel};
 
-fn main() {
-    // let input = fs::read_to_string("input_example1.txt").unwrap();
-    let input = fs::read_to_string("input1.txt").unwrap();
-    // let input = fs::read_to_string("my_input.txt").unwrap();
+use threadpool::ThreadPool;
 
+fn test(input: String) {
     let sum: usize = input
         .lines()
-        .map(|line| {
+        .enumerate()
+        .map(|(i, line)| {
+            println!("line {}", i);
             let mut split = line.split_whitespace();
-            // split by known clusters
-            let mask: Vec<char> = split.next().unwrap().chars().collect();
-            let clusters: Vec<usize> = split
-                .next()
-                .unwrap()
+            let m = split.next().unwrap();
+            let c = split.next().unwrap();
+            let mask_1: Vec<char> = repeat(m)
+                .take(5)
+                .collect::<Vec<&str>>()
+                .join("?")
+                .chars()
+                .collect();
+            let clusters_1: Vec<usize> = repeat(c)
+                .take(5)
+                .collect::<Vec<&str>>()
+                .join(",")
                 .split(",")
                 .map(|n| n.parse().unwrap())
                 .collect();
-
             println!("==================== Start of new line > {}", line);
-            println!("mask {}", mask.iter().collect::<String>());
-            let mut sums = Vec::new();
-            segment(0, &clusters, &mask, &mut sums, "");
-            println!("sums {:?}", sums);
-            sums.last().unwrap().clone()
+            println!("mask {}", mask_1.iter().collect::<String>());
+            println!("cluster {:?}", clusters_1);
+            let combs_1 = segment(0, &clusters_1, &mask_1, "");
+            println!("combs {}", combs_1);
+            // println!("combs {}", combs_2);
+            // println!("total {}", total);
+            // return total;
+            combs_1
         })
         .sum();
 
-    println!("Part one: {sum}");
-    //
-    // calc possibilities in each segment
-    //
-    // multiply possibilities together
+    println!("sum {}", sum)
 }
 
-fn segment(
-    i: usize,
-    clusters: &Vec<usize>,
-    mask: &[char],
-    sums: &mut Vec<usize>,
-    before: &str,
-) -> bool {
+fn main() {
+    // let input = fs::read_to_string("input_example1.txt").unwrap();
+    // let input = fs::read_to_string("input1.txt").unwrap();
+    let input = fs::read_to_string("my_input.txt").unwrap();
+    
+    test(input);
+    return;
+
+    let pool = ThreadPool::new(15);
+
+    let (tx, rx) = channel();
+
+    let mut job_count = 0;
+    input.lines().enumerate().for_each(|(i, line)| {
+        println!("line {}", i);
+        let mut split = line.split_whitespace();
+        let m = split.next().unwrap();
+        let c = split.next().unwrap();
+        // let mask_1: Vec<char> = repeat(m)
+        //     .take(1)
+        //     .collect::<Vec<&str>>()
+        //     .join("?")
+        //     .chars()
+        //     .collect();
+        // let clusters_1: Vec<usize> = repeat(c)
+        //     .take(1)
+        //     .collect::<Vec<&str>>()
+        //     .join(",")
+        //     .split(",")
+        //     .map(|n| n.parse().unwrap())
+        //     .collect();
+        // let mask_2: Vec<char> = repeat(m)
+        //     .take(2)
+        //     .collect::<Vec<&str>>()
+        //     .join("?")
+        //     .chars()
+        //     .collect();
+        // let clusters_2: Vec<usize> = repeat(c)
+        //     .take(2)
+        //     .collect::<Vec<&str>>()
+        //     .join(",")
+        //     .split(",")
+        //     .map(|n| n.parse().unwrap())
+        //     .collect();
+        let mask_5: Vec<char> = repeat(m)
+            .take(5)
+            .collect::<Vec<&str>>()
+            .join("?")
+            .chars()
+            .collect();
+        let clusters_5: Vec<usize> = repeat(c)
+            .take(5)
+            .collect::<Vec<&str>>()
+            .join(",")
+            .split(",")
+            .map(|n| n.parse().unwrap())
+            .collect();
+
+        job_count += 1;
+
+        let tx = tx.clone();
+        pool.execute(move || {
+            // println!("==================== Start of new line > {}", line);
+            // println!("mask {}", mask_2.iter().collect::<String>());
+            // println!("cluster {:?}", clusters_2);
+            // let combs_1 = segment(0, &clusters_1, &mask_1, "");
+            // let combs_2 = segment(0, &clusters_2, &mask_2, "");
+            // println!("combs {}", combs_1);
+            // println!("combs {}", combs_2);
+            // let rem = combs_2 % combs_1;
+            // if rem == 0 {
+            //     let factor = combs_2 / combs_1;
+            //     let total = combs_1 * usize::pow(factor, 4);
+            //     // println!("total {}", total);
+            //     // return total;
+            //     tx.send(total).unwrap();
+            // }
+            // println!("rem {}", rem);
+
+            let combs = segment(0, &clusters_5, &mask_5, "");
+            // println!("combs {}", combs);
+            tx.send(combs).unwrap();
+        });
+    });
+
+    let mut received = 0;
+    let mut sum = 0;
+    while received < job_count {
+        let result = rx.recv().unwrap();
+        println!("Got: {}, received {}", result, received);
+        received += 1;
+        sum += result;
+    }
+
+    println!("sum {}", sum);
+}
+
+fn segment(i: usize, clusters: &Vec<usize>, mask: &[char], before: &str) -> usize {
     // println!("-------- Start of {}", i);
     if i >= clusters.len() {
         if !mask.contains(&'#') {
@@ -48,15 +144,11 @@ fn segment(
             //     "end  {}",
             //     before.to_string() + &mask.iter().collect::<String>()
             // );
-            return true;
+            return 1;
         }
-        return false;
+        return 0;
     }
     // println!("mask {:?}", mask);
-
-    if sums.get(i).is_none() {
-        sums.push(0);
-    }
 
     let size = clusters[i];
     // println!("size {}", size);
@@ -66,34 +158,26 @@ fn segment(
         piece.push_back('.');
     }
 
-    let mut one_was_valid = false;
+    let mut seg_total = 0;
     loop {
         // can this piece go here?
         let valid = is_valid_pos(mask, &piece);
         let next_before = before.to_string() + &piece.iter().collect::<String>();
         if valid {
-            let seg_valid = segment(
+            seg_total += segment(
                 i + 1,
                 clusters,
                 &mask[piece.len()..mask.len()],
-                sums,
                 &next_before,
             );
-
-            if seg_valid {
-                one_was_valid = true;
-                sums[i] += 1;
-            }
-        } else {
         }
-
         piece.push_front('.');
         if piece.len() > mask.len() {
             break;
         }
     }
 
-    return one_was_valid;
+    return seg_total;
 }
 
 fn is_valid_pos(mask: &[char], piece: &VecDeque<char>) -> bool {
