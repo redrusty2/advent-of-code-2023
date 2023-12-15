c49cf5b2bd3d9e61808e84eaa0e005d1e94da1ef
Caleb Rust <caleb.rust@consilium.technology>
2023-12-15 17:23:13 +1030

    day15 part2 with map

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -6,29 +6,15 @@ fn main() {
     let input = fs::read_to_string("input1.txt").unwrap();
     // let input = fs::read_to_string("input_example1.txt").unwrap();
 
-    // let sum: usize = input
-    //     .split(",")
-    //     .map(|seq| {
-    //         seq.chars()
-    //             .filter(|c| c != &'\n')
-    //             .map(|c| c as usize)
-    //             .fold(0, |acc, c| ((acc + c) * 17) % 256)
-    //     })
-    //     .sum();
-
-    // println!("Part one: {}", sum);
-    //
-
-    let mut boxes: Vec<Vec<(&str, usize)>> = Vec::new();
+    let mut boxes: Vec<IndexMap<&str, usize>> = Vec::new();
     for _ in 0..256 {
-        boxes.push(Vec::new());
+        boxes.push(IndexMap::new());
     }
 
     for seq in input.split(",") {
-        println!("================== seq {}", seq);
         let op_idx = match &seq[seq.len() - 1..seq.len()] {
             "-" => seq.len() - 1,
-            _ => seq.len() - 2
+            _ => seq.len() - 2,
         };
         let lens = &seq[0..op_idx];
         let op = &seq[op_idx..op_idx + 1];
@@ -36,44 +22,33 @@ fn main() {
 
         match op {
             "-" => {
-                let pos = boxes[box_idx].iter().position(|(l, _)| l == &lens);
-                if let Some(p) = pos {
-                    boxes[box_idx].remove(p);
-                }
+                boxes[box_idx].shift_remove(lens);
             }
             "=" => {
-                let pos = boxes[box_idx].iter().position(|(l, _)| l == &lens);
                 let focal_len = seq[op_idx + 1..seq.len()].parse::<usize>().unwrap();
-                if let Some(p) = pos {
-                    boxes[box_idx][p] = (lens, focal_len);
-                } else {
-                    boxes[box_idx].push((lens, focal_len));
+                if boxes[box_idx].contains_key(lens) {
+                    *boxes[box_idx].get_mut(lens).unwrap() = focal_len;
+                }
+                else{
+                    boxes[box_idx].insert(lens, focal_len);
                 }
             }
             _ => {
                 panic!("Unknown op {} seq {}", op, seq);
             }
         }
-
-        // for i in 0..4 {
-        //     let bx = &boxes[i];
-        //     println!("box {:?}", bx);
-        // }
     }
 
-    // for i in 0..4 {
-    //     let bx = &boxes[i];
-    //     println!("box {:?}", bx);
-    // }
-    for bx in boxes.iter() {
-        println!("box {:?}", bx);
-    }
-
-    let sum: usize = boxes.iter().enumerate().map(|(box_idx, bx)| {
-        bx.iter().enumerate().map(move |(slot, (_, focal_len))| {
-            (box_idx + 1) * (slot + 1) * focal_len
+    let sum: usize = boxes
+        .iter()
+        .enumerate()
+        .map(|(box_idx, bx)| {
+            bx.iter()
+                .enumerate()
+                .map(move |(slot, (_, focal_len))| (box_idx + 1) * (slot + 1) * focal_len)
         })
-    }).flatten().sum();
+        .flatten()
+        .sum();
 
     println!("Part 2: {}", sum);
 }
